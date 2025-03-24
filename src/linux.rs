df801bb76386dcacd84e7a35aa2e56b1218ed6de
Silvan Schmidt <mail@silvanschmidt.ch>
2025-03-24 21:27:53 +0100

    feat: respect XDG_ env variables

--- src/linux.rs
+++ src/linux.rs
@@ -6,19 +6,20 @@ pub fn config_path(
     #[allow(unused)] organization_name: String,
     app_name: String,
 ) -> Result<PathBuf, Error> {
-    home_path().map(|home_path| home_path.join(".config").join(convert_app_name(app_name)))
+    Ok(std::env::var("XDG_CONFIG_HOME")
+        .map(|raw_config_home_path| Path::new(&raw_config_home_path).to_path_buf())
+        .unwrap_or(home_path()?.join(".config"))
+        .join(convert_app_name(app_name)))
 }
 
 pub fn data_path(
     #[allow(unused)] organization_name: String,
     app_name: String,
 ) -> Result<PathBuf, Error> {
-    home_path().map(|home_path| {
-        home_path
-            .join(".local")
-            .join("share")
-            .join(convert_app_name(app_name))
-    })
+    Ok(std::env::var("XDG_DATA_HOME")
+        .map(|raw_data_home_path| Path::new(&raw_data_home_path).to_path_buf())
+        .unwrap_or(home_path()?.join(".local/share"))
+        .join(convert_app_name(app_name)))
 }
 
 fn home_path() -> Result<PathBuf, Error> {
@@ -44,9 +45,25 @@ mod tests {
     }
 
     #[test]
+    fn xdg_config_path_test() {
+        unsafe { std::env::set_var("XDG_CONFIG_HOME", "/") };
+        let expected = Path::new("/standard-directories").to_path_buf();
+        let actual = config_path("Foo".to_string(), "Standard Directories".to_string()).unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
     fn data_path_test() {
         unsafe { std::env::set_var("HOME", "/home/john") };
         let expected = Path::new("/home/john/.local/share/standard-directories").to_path_buf();
+        let actual = data_path("Foo".to_string(), "Standard Directories".to_string()).unwrap();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn xdg_data_path_test() {
+        unsafe { std::env::set_var("XDG_DATA_HOME", "/") };
+        let expected = Path::new("/standard-directories").to_path_buf();
         let actual = data_path("Foo".to_string(), "Standard Directories".to_string()).unwrap();
         assert_eq!(expected, actual);
     }
