a4f96cea2e0a9e21b8e70393bee2ed62406103ab
Silvan Schmidt <mail@silvanschmidt.ch>
2025-03-25 19:57:26 +0100

    refactor: forward function calls from lib.rs to linux.rs

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,27 @@
 compile_error!("your operating system is not supported yet.");
 
 mod error;
+use std::path::PathBuf;
+
 pub use error::Error;
 
 #[cfg(target_os = "linux")]
 mod linux;
-#[cfg(target_os = "linux")]
-pub use linux::{config_path, data_path};
+
+/// Get the configuration directory.
+pub fn config_path(
+    #[cfg_attr(target_os = "linux", allow(unused))] organization_name: String,
+    app_name: String,
+) -> Result<PathBuf, Error> {
+    #[cfg(target_os = "linux")]
+    linux::config_path(app_name)
+}
+
+/// Get the data directory.
+pub fn data_path(
+    #[cfg_attr(target_os = "linux", allow(unused))] organization_name: String,
+    app_name: String,
+) -> Result<PathBuf, Error> {
+    #[cfg(target_os = "linux")]
+    linux::data_path(app_name)
+}
--- src/linux.rs
+++ src/linux.rs
@@ -2,20 +2,14 @@ use std::path::{Path, PathBuf};
 
 use crate::Error;
 
-pub fn config_path(
-    #[allow(unused)] organization_name: String,
-    app_name: String,
-) -> Result<PathBuf, Error> {
+pub fn config_path(app_name: String) -> Result<PathBuf, Error> {
     Ok(std::env::var("XDG_CONFIG_HOME")
         .map(|raw_config_home_path| Path::new(&raw_config_home_path).to_path_buf())
         .unwrap_or(home_path()?.join(".config"))
         .join(convert_app_name(app_name)))
 }
 
-pub fn data_path(
-    #[allow(unused)] organization_name: String,
-    app_name: String,
-) -> Result<PathBuf, Error> {
+pub fn data_path(app_name: String) -> Result<PathBuf, Error> {
     Ok(std::env::var("XDG_DATA_HOME")
         .map(|raw_data_home_path| Path::new(&raw_data_home_path).to_path_buf())
         .unwrap_or(home_path()?.join(".local/share"))
@@ -30,41 +24,4 @@ fn home_path() -> Result<PathBuf, Error> {
 
 fn convert_app_name(app_name: String) -> String {
     app_name.to_lowercase().replace(" ", "-")
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn config_path_test() {
-        unsafe { std::env::set_var("HOME", "/home/john") };
-        let expected = Path::new("/home/john/.config/standard-directories").to_path_buf();
-        let actual = config_path("Foo".to_string(), "Standard Directories".to_string()).unwrap();
-        assert_eq!(expected, actual);
-    }
-
-    #[test]
-    fn xdg_config_path_test() {
-        unsafe { std::env::set_var("XDG_CONFIG_HOME", "/") };
-        let expected = Path::new("/standard-directories").to_path_buf();
-        let actual = config_path("Foo".to_string(), "Standard Directories".to_string()).unwrap();
-        assert_eq!(expected, actual);
-    }
-
-    #[test]
-    fn data_path_test() {
-        unsafe { std::env::set_var("HOME", "/home/john") };
-        let expected = Path::new("/home/john/.local/share/standard-directories").to_path_buf();
-        let actual = data_path("Foo".to_string(), "Standard Directories".to_string()).unwrap();
-        assert_eq!(expected, actual);
-    }
-
-    #[test]
-    fn xdg_data_path_test() {
-        unsafe { std::env::set_var("XDG_DATA_HOME", "/") };
-        let expected = Path::new("/standard-directories").to_path_buf();
-        let actual = data_path("Foo".to_string(), "Standard Directories".to_string()).unwrap();
-        assert_eq!(expected, actual);
-    }
 }
